caeb05f3364afca5412e7bcdcfac9f799ab2060d
bbrvi1993@gmail.com <bbrvi1993@gmail.com>
2020-12-25 01:12:36 +0700

    Remove comments

--- src/components/MoviesApp/MoviesApp.jsx
+++ src/components/MoviesApp/MoviesApp.jsx
@@ -37,8 +37,6 @@ export default class MoviesApp extends Component {
   render() {
     const { currentTab, genres } = this.state;
 
-    console.log(currentTab);
-
     return (
       <GenresProvider value={genres}>
         <Layout>
